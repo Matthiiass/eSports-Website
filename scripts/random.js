8ef9c4f16e4f5a971add30173566e4904381e720
Matthiiass <matthewgregory278@gmail.com>
2022-10-03 15:19:52 +0100

    Finished off the homepage and organised some stuff

--- scripts/random.js
+++ scripts/random.js
@@ -1,3 +1,15 @@
 function buttonHover() {
-    document.getElementById('readMoreButt').style.color = '#cf5fa1'
+    document.getElementById('readMoreButt').style.backgroundColor = '#cf5fa1'
+};
+
+function buttonUnhover() {
+    document.getElementById('readMoreButt').style.backgroundColor = '#b8518e'
+};
+
+function buttonHover2() {
+    document.getElementById('buyNowButton').style.backgroundColor = '#cf5fa1'
+};
+
+function buttonUnhover2() {
+    document.getElementById('buyNowButton').style.backgroundColor = '#b8518e'
 };
